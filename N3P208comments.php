028f90e649b824ff0cbca667ccae60848f4233fe
OnlyRichard20 <72759533+OnlyRichard20@users.noreply.github.com>
2020-11-06 12:49:30 +0100

    Add files via upload

---
+++ N3P208comments.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "
+    <ol>
+        <li>Conexión BBDD</li>
+        <li>For each </li>
+        <li>mysqli_fetch_assoc($result))</li>
+        <li>mysqli_select_db</li>
+        <li>Creación de tablas con php</li>
+        <li>Función extract()</li>
+        <li>Mostar datos con php</li>
+        <li>Utilizar un get para mostrar datos</li>
+        <li>Inserción de datos con php</li>
+        <li>Query </li>
+    </ol>
+    
+    <p>Nota del documento: 9</p>
+    <p>Nota de la explicación del profesor: 10</p>
+    <p>Nota del alumno (Ricardo Paredes Ramos): 10</p>
+    <p>Explicación mejoras: Nada que mejorar</p>";
+
+?>
